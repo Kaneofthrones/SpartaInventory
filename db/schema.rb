211ce9bd7a2e9aff3beef6f9fbe49675fcff42b6
Divesh Mavji <divesh987@hotmail.co.uk>
2017-09-04 11:36:33 +0100

    migrated the new column migration;

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170731101825) do
+ActiveRecord::Schema.define(version: 20170904103148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170731101825) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.text     "serial"
+    t.date     "due_date"
   end
 
   create_table "logs", force: :cascade do |t|
