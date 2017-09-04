0d179743a661f4bf3db3538e6873df313b63dd63
Sabirin Munye <sabirinmunye@gmail.com>
2017-09-04 14:53:35 +0100

    chan=nges to the white spacing

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
 
-	
 	has_many :logs
 	has_one :current, -> { where return_date: nil }, class_name: "Log"
 	validates :description, presence: true
--- app/models/log.rb
+++ app/models/log.rb
@@ -1,8 +1,7 @@
 class Log < ApplicationRecord
   
   belongs_to :item
-  validates :title, presence: true
-
+  
   attr_accessor :borrower, :lender , :returned_to
   
 end
