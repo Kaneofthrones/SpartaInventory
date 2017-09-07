bf6edd63fa483f1b262370daca68ec3c612ef0ac
Tech-A03 <tech-a03@Tech-A03s-MacBook-Air-2.local>
2017-09-07 14:01:26 +0100

    Changed slack notifier webhook url to an environmental variable

--- config/initializers/rufus.rb
+++ config/initializers/rufus.rb
@@ -2,10 +2,9 @@ require 'rufus-scheduler'
 # Let's use the rufus-scheduler singleton
 s = Rufus::Scheduler.singleton
 # Slack notifications linked to webhook that is directed to the channel random with the name overdue notifier
-SLACK_NOTIFIER1 = Slack::Notifier.new(
-	"https://hooks.slack.com/services/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", channel: "#random", username: "OVERDUE NOTIFIER")
+SLACK_NOTIFIER1 = Slack::Notifier.new(ENV["webhook"], username: "OVERDUE NOTIFIER")
 # Every interval run through all the items and compare due dates of current logs to the current date. 
-s.cron '31 13 * * *'  do
+s.cron '00 15 * * *'  do
 	items = Item.all
 	items.each do |item|
 		if item.current
